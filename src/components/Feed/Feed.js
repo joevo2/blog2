b8c010fac524de2bc1aa6be22b0c53670d374a3c
Joel Yek Zong Yong <joel.yekzongyong@baesystems.com>
2019-04-11 14:24:30 +0800

    removed read button

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -19,8 +19,8 @@ const Feed = ({ edges }) => (
         <h2 className={styles['feed__item-title']}>
           <Link className={styles['feed__item-title-link']} to={edge.node.fields.slug}>{edge.node.frontmatter.title}</Link>
         </h2>
-        <p className={styles['feed__item-description']}>{edge.node.frontmatter.description}</p>
-        <Link className={styles['feed__item-readmore']} to={edge.node.fields.slug}>Read</Link>
+        <Link className={styles['feed__item-description']} style={{ color: 'black' }} to={edge.node.fields.slug}>{edge.node.frontmatter.description}</Link>
+        {/* <Link className={styles['feed__item-readmore']} to={edge.node.fields.slug}>Read</Link> */}
       </div>
     ))}
   </div>
