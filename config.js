1a0279ce4bf54fe5639e357784ce60b9407c9b07
Joel Yek <joel@Joels-MacBook-Air.local>
2019-04-11 22:55:03 +0800

    Added all article at tags, fixed author bio

--- config.js
+++ config.js
@@ -25,7 +25,7 @@ module.exports = {
   author: {
     name: 'Joel Yek',
     photo: '/photo.jpg',
-    bio: 'Here is where I do my long form post, anything else tweet me at @joveo2 #react #reactnative #node #developer #anime',
+    bio: 'Here is where I do my long form post, anything else tweet me',
     contacts: {
       email: 'joevo2@gmail.com',
       telegram: '',
--- src/components/Post/Tags/Tags.js
+++ src/components/Post/Tags/Tags.js
@@ -5,6 +5,9 @@ import styles from './Tags.module.scss';
 const Tags = ({ tags, tagSlugs }) => (
   <div className={styles['tags']}>
     <ul className={styles['tags__list']}>
+      <li className={styles['tags__list-item']}>
+        <Link className={styles['tags__list-item-link']} to="/">All Articles</Link>
+      </li>
       {tagSlugs.map((slug, i) => (
         <li className={styles['tags__list-item']} key={tags[i]}>
           <Link to={slug} className={styles['tags__list-item-link']}>
