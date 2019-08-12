b59e056798ae860c9743a65974e86864addc5684
Joel Yek <joevo2@gmail.com>
2019-08-12 16:29:10 +0800

    update "all article" to /blog

--- src/components/Post/Tags/Tags.js
+++ src/components/Post/Tags/Tags.js
@@ -6,7 +6,7 @@ const Tags = ({ tags, tagSlugs }) => (
   <div className={styles['tags']}>
     <ul className={styles['tags__list']}>
       <li className={styles['tags__list-item']}>
-        <Link className={styles['tags__list-item-link']} to="/">All Articles</Link>
+        <Link className={styles['tags__list-item-link']} to="/blog">All Articles</Link>
       </li>
       {tagSlugs.map((slug, i) => (
         <li className={styles['tags__list-item']} key={tags[i]}>
