0f91de9c624fac68af82b28c5d9f3dc6d164a6e0
Joel Yek Zong Yong <joel.yekzongyong@Imp.net>
2019-08-15 13:32:20 +0800

    added copyright text

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   url: 'https://joevo2.com',
   title: 'Joel Yek',
   subtitle: 'Here is where I do my long form post, anything else tweet me at @joveo2 #react #reactnative #node #developer #anime',
-  copyright: '',
+  copyright: `Made with ❤️ from Malaysia, ${(new Date()).getFullYear()}`,
   disqusShortname: '',
   postsPerPage: 5,
   googleAnalyticsId: 'UA-5243219-11',
